cb36b92abc4c8ef012bd81d7797e1cfd241d401c
Ashish <ashishjaitiwari15112000@gmail.com>
2020-07-03 02:22:53 +0530

    added someSorts.cpp

---
+++ someSorts.cpp
@@ -0,0 +1,25 @@
+/*
+Some very interesting sorting algorithms are-
+
+        1.)Hitler Sort-Hitler Sort is the best sorting algorithm as 
+        it works in O(1).In hitler sort,you basically name the array-"Jews",
+        and then delete it.
+
+        2.)Modi Sort- Modiji is sometimes compared to hitler but ironically
+        modi sort is slowest as it works in O(n!).In modi sort ,you basically
+        keep trying different combinations and when array doesn't get sorted quickly
+        ,you simply say-"Are congress ne 70 saal me konsa array sort kiya".
+
+        3.)SBI sort- This is even faster sorting algorithm as it works in O(0)!!!
+        Yes you heard it write,O(0)!!!!
+        In SBI sort,you dont sort the array at all. You just sit and tell the array 
+        that-"Lunch ka time ho gaya hai,baad me aana!!"
+
+        4.) CAA-NRC sort- This sort works in O(n).
+        Basically you first traverse the array and push any element thats out of order in a second array.
+        Then you traverse the second the array and delete all the muslim elements.
+        After this,you push the second array back in first array.Bazinga!!!
+
+        5.)
+
+*/
