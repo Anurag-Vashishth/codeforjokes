68e445b406328f3af3937c2d2916d5f2ba3d86e4
Anurag Vashishth <avs1582@gmail.com>
2020-07-03 13:59:36 +0530

    Adding comments

--- someSorts.cpp
+++ someSorts.cpp
@@ -10,15 +10,25 @@ Some very interesting sorting algorithms are-
         keep trying different combinations and when array doesn't get sorted quickly
         ,you simply say-"Are congress ne 70 saal me konsa array sort kiya".
 
+        This method of sorting is also called as blame referencing, where the current admin
+        references the blame to the previous admin.
+
         3.)SBI sort- This is even faster sorting algorithm as it works in O(0)!!!
         Yes you heard it write,O(0)!!!!
         In SBI sort,you dont sort the array at all. You just sit and tell the array 
         that-"Lunch ka time ho gaya hai,baad me aana!!"
 
+
         4.) CAA-NRC sort- This sort works in O(n).
         Basically you first traverse the array and push any element thats out of order in a second array.
         Then you traverse the second the array and delete all the muslim elements.
         After this,you push the second array back in first array.Bazinga!!!
+
+        To ease this process the CAA-NRC sort passes the elements of the array through a bool function
+        named isMuslim(), if the element on passing it through this function gives out true, is added 
+        to another array named second_degree_citizen, and when all the elements are passed through the function
+        and the new array is complete, the admin and only the admin is allowed to delete it.
+        Now the original array is sorted and you finally get the sorted array named hindu_rashtra 
 
         5.)Therapist-sort- This is by far the best sorting algorithm ever created.
         It works in O(-n)!!!!!!!!!!!!!!!
@@ -29,6 +39,9 @@ Some very interesting sorting algorithms are-
         is fine and tells you that ,"Its okay to be sad sometimes.Its okay to feel lonely or incompetent or
         overwhelmed or insecure by this rapidly changing competetive world. YOU ARE ENOUGH."Then it iterates
         through all your problems and solves them.This sort is very wholesome. 
+
+        Although as helpful this sorting method is, it is not free, because not every array deserves to be 
+        this sorted. 
 
         
 
